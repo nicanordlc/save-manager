397f29e242b67c65aaf56ea58b14554b0aeff54a
cabaalexander <cabaalexander321@gmail.com>
2024-04-11 15:30:46 -0400

    bootstrap cleanup

--- app.go
+++ app.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 )
 
 // App struct
@@ -19,12 +18,4 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
-}
-
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	if name == "" {
-		name = "World"
-	}
-	return fmt.Sprintf("Hello %s, It's show time!", name)
 }
