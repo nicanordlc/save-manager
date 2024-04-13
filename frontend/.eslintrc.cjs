52573efc3199dbc9fcc7db3e423bd934d0e10027
cabaalexander <cabaalexander321@gmail.com>
2024-04-13 00:15:42 -0400

    add prettier + basic config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,3 +1,4 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -6,16 +7,23 @@ module.exports = {
     'airbnb',
     'airbnb-typescript',
     'airbnb/hooks',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './frontend/tsconfig.json'
   },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    'prettier'
+  ],
   rules: {
+    'react/react-in-jsx-scope': 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
