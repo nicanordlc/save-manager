25dd5265343a3c10c42a254d2764765d5fe1cc26
cabaalexander <cabaalexander321@gmail.com>
2024-04-16 19:07:07 -0400

    add routing

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,12 +1,28 @@
 import { render, screen } from '@testing-library/react';
+import { HashRouter, MemoryRouter } from 'react-router-dom';
 import App from '@/App';
 
 describe('App.tsx', () => {
   it('renders hello world', () => {
-    render(<App />);
+    render(
+      <HashRouter>
+        <App />
+      </HashRouter>
+    );
 
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
-      'Vite + React'
+      'Hello World'
+    );
+  });
+
+  it('renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/bananas']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'Not Found'
     );
   });
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,14 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import viteLogo from '@/assets/vite.svg';
 import '@/App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from '@/pages/Home';
+import NotFound from '@/pages/NotFound';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((_count) => _count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
