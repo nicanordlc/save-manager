b4397cbcb1664d7557665b97d3be7cf1307f683d
cabaalexander <cabaalexander321@gmail.com>
2024-04-17 19:02:01 -0400

    set hot reloading (port explicitly set)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,6 +16,9 @@ const ignoreTests = [
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 11030,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
