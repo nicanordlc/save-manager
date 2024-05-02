719af43a6dd4c5bd112ca95ab45cf85cc3e115ef
cabaalexander <cabaalexander321@gmail.com>
2024-05-02 01:37:59 -0400

    pretty no game text

--- frontend/src/components/NoGame.tsx
+++ frontend/src/components/NoGame.tsx
@@ -1,7 +1,14 @@
 import { Typography } from "@material-tailwind/react";
 
 const NoGame = () => {
-  return <Typography>Add your games 👾</Typography>;
+  return (
+    <Typography
+      className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 "
+      variant="lead"
+    >
+      Add your games 👾
+    </Typography>
+  );
 };
 
 export default NoGame;
